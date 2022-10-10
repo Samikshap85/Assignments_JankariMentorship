c79bd8af7d5ffc51f147cbcfc456a6c90c637d01
samikshapaudel <samikshapaudel22@gmail.com>
2022-10-10 13:38:21 +0545

    Error fixed on fetch Age from Name

--- fetchName.mjs
+++ fetchName.mjs
@@ -1,12 +1,14 @@
 //WAP to display age according to the given name using async and await throught API call
 import fetch from "node-fetch";
-async function Age(argName){
+async function Age(argname){
 
-  const responseObject = await fetch(`https://api.agify.io/?name=${name}`)
-  const textData = await responseObject.text();
-  const {age ,name} = textData;
+  const responseObject = await fetch(`https://api.agify.io/?name=${argname}`)
+  const result = await responseObject.json();
+  const {age ,name} = result;
   console.log(`Age of ${name} is :`,age);
 }
-Age(argName);
+Age("Samiksha");
+
+
 
   
